95c11f77d0af7564cc6a5660459f38ade496775d
Yuuon <yuuon.himemiya@gmail.com>
2016-11-16 08:15:40 +0100

    gg

--- ProjetMCS/app/src/main/java/com/himenomiya/www/projetmcs/recording.java
+++ ProjetMCS/app/src/main/java/com/himenomiya/www/projetmcs/recording.java
@@ -1,9 +1,11 @@
 package com.himenomiya.www.projetmcs;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,7 +17,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import static android.Manifest.permission.RECORD_AUDIO;
@@ -24,11 +30,21 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 
 public class recording extends AppCompatActivity {
-    String AudioSavePathInDevice = null;
-    MediaRecorder mediaRecorder ;
+    private static final int RECORDER_BPP = 16;
+    private static final int RECORDER_SAMPLERATE = 44100;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+    private AudioRecord recorder = null;
+    private int bufferSize = 0;
+    private Thread recordingThread = null;
+    private Thread playingThread = null;
+    private boolean isRecording = false;
+
+
     public static final int RequestPermissionCode = 1;
-    MediaPlayer mediaPlayer ;
-    Button buttonStart,buttonStop,buttonPlay,buttonRecStop;
+
+    Button buttonStart,buttonStop,buttonPlay;
     String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,79 +57,207 @@ public class recording extends AppCompatActivity {
         buttonStart = (Button) findViewById(R.id.buttonStart);
         buttonStop = (Button) findViewById(R.id.buttonstop);
         buttonPlay = (Button) findViewById(R.id.buttonPlay);
-        buttonRecStop = (Button) findViewById(R.id.buttonRecStop);
+
         buttonStart.setEnabled(true);
         buttonStop.setEnabled(false);
-        buttonRecStop.setEnabled(false);
-        File recExist = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
-                message + ".3gp");
-        AudioSavePathInDevice =
-                Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
-                        message + ".3gp";
+
+        File recExist = new File(getFilename());
+
         if(recExist.exists() && !recExist.isDirectory()) {
             buttonPlay.setEnabled(true);
         } else {
             buttonPlay.setEnabled(false);
         }
+        bufferSize = AudioRecord.getMinBufferSize(8000,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
 
 
-        /*
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+
+
+
+    }
+
+
+
+
+
+    private void startRecording(){
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,RECORDER_AUDIO_ENCODING, bufferSize);
+
+        int i = recorder.getState();
+        if(i==1)
+            recorder.startRecording();
+
+        isRecording = true;
+
+        recordingThread = new Thread(new Runnable() {
+
             @Override
-            public void onClick(View view) {
-
-
-
+            public void run() {
+                writeAudioDataToFile();
             }
-        });
-        */
-        buttonStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaRecorder.stop();
-                buttonStop.setEnabled(false);
-                buttonPlay.setEnabled(true);
-                buttonStart.setEnabled(true);
-                buttonRecStop.setEnabled(false);
+        },"AudioRecorder Thread");
 
+        recordingThread.start();
+    }
 
-                Toast.makeText(recording.this, "Recording Completed",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
+    private String getFilename(){
+        return (Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                message + ".wav");
+    }
 
-        buttonPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) throws IllegalArgumentException,
-                    SecurityException, IllegalStateException {
+    private void writeAudioDataToFile(){
+        byte data[] = new byte[bufferSize];
+        String filename = getTempFilename();
+        FileOutputStream os = null;
 
-                buttonStop.setEnabled(false);
-                buttonStart.setEnabled(false);
-                buttonRecStop.setEnabled(true);
+        try {
+            os = new FileOutputStream(filename);
+        } catch (FileNotFoundException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-                mediaPlayer = new MediaPlayer();
-                try {
-                    mediaPlayer.setDataSource(AudioSavePathInDevice);
-                    mediaPlayer.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        int read = 0;
+
+        if(null != os){
+            while(isRecording){
+                read = recorder.read(data, 0, bufferSize);
+
+                if(AudioRecord.ERROR_INVALID_OPERATION != read){
+                    try {
+                        os.write(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-
-                mediaPlayer.start();
-                Toast.makeText(recording.this, "Recording Playing",
-                        Toast.LENGTH_LONG).show();
             }
-        });
 
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private String getTempFilename(){
+        return (Environment.getExternalStorageDirectory().getAbsolutePath() + "/temp_rec.raw" );
     }
-    public void MediaRecorderReady(){
-        mediaRecorder=new MediaRecorder();
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+
+    private void stopRecording(){
+        if(null != recorder){
+            isRecording = false;
+
+            int i = recorder.getState();
+            if(i==1)
+                recorder.stop();
+            recorder.release();
+
+            recorder = null;
+            recordingThread = null;
+        }
+
+        copyWaveFile(getTempFilename(),getFilename());
+        deleteTempFile();
     }
+
+    private void deleteTempFile() {
+        File file = new File(getTempFilename());
+
+        file.delete();
+    }
+
+    private void copyWaveFile(String inFilename,String outFilename){
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = RECORDER_SAMPLERATE;
+        int channels = 2;
+        long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
+
+        byte[] data = new byte[bufferSize];
+
+        try {
+            in = new FileInputStream(inFilename);
+            out = new FileOutputStream(outFilename);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+                    longSampleRate, channels, byteRate);
+
+            while(in.read(data) != -1){
+                out.write(data);
+            }
+
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void WriteWaveFileHeader(
+            FileOutputStream out, long totalAudioLen,
+            long totalDataLen, long longSampleRate, int channels,
+            long byteRate) throws IOException {
+
+        byte[] header = new byte[44];
+
+        header[0] = 'R'; // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f'; // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1; // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8); // block align
+        header[33] = 0;
+        header[34] = RECORDER_BPP; // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+        out.write(header, 0, 44);
+    }
+
     private void requestPermission() {
         ActivityCompat.requestPermissions(recording.this, new String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
     }
@@ -148,33 +292,13 @@ public class recording extends AppCompatActivity {
         return result == PackageManager.PERMISSION_GRANTED &&
                 result1 == PackageManager.PERMISSION_GRANTED;
     }
-    public String getDiskCacheDir(Context context) {
-        String cachePath = null;
-        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
-                || !Environment.isExternalStorageRemovable()) {
-            cachePath = context.getExternalCacheDir().getPath();
-        } else {
-            cachePath = context.getCacheDir().getPath();
-        }
-        return cachePath;
-    }
+
     public void startRecording(View view) {
         if(checkPermission()) {
 
 
 
-            MediaRecorderReady();
-
-            try {
-                mediaRecorder.prepare();
-                mediaRecorder.start();
-            } catch (IllegalStateException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            startRecording();
 
             buttonStart.setEnabled(false);
             buttonStop.setEnabled(true);
@@ -187,15 +311,72 @@ public class recording extends AppCompatActivity {
     }
     public void stopRecording(View view) {
         buttonStop.setEnabled(false);
+        buttonPlay.setEnabled(true);
         buttonStart.setEnabled(true);
-        buttonRecStop.setEnabled(false);
+
+
+
+        Toast.makeText(recording.this, "Recording Completed",
+                Toast.LENGTH_LONG).show();
+
+
+
+
+        stopRecording();
+    }
+
+    public  void startPlaying(View view) {
+        buttonStop.setEnabled(false);
+        buttonStart.setEnabled(true);
         buttonPlay.setEnabled(true);
 
-        if(mediaPlayer != null){
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            MediaRecorderReady();
+
+        Toast.makeText(recording.this, "Recording Playing",
+                Toast.LENGTH_LONG).show();
+        playingThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                playWav();
+            }
+        },"AudioTrack Thread");
+
+        playingThread.start();
+
+
+    }
+
+
+    public void playWav(){
+        int minBufferSize = AudioTrack.getMinBufferSize(8000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        int bufferSize = 512;
+        AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 44100 * 2, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize, AudioTrack.MODE_STREAM);
+
+
+        int i = 0;
+        byte[] s = new byte[bufferSize];
+        try {
+            FileInputStream fin = new FileInputStream(getFilename());
+            DataInputStream dis = new DataInputStream(fin);
+
+            at.play();
+            while((i = dis.read(s, 0, bufferSize)) > -1){
+                at.write(s, 0, i);
+
+            }
+            at.stop();
+            at.release();
+            dis.close();
+            fin.close();
+
+        } catch (FileNotFoundException e) {
+            // TODO
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO
+            e.printStackTrace();
         }
+
     }
 }
 
